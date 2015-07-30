76643b200e95ad04d5e21a489a92171a9d8b3c03
Hung Nguyen Viet <hvn@familug.org>
2015-07-30 16:27:39 +0800

    typo

--- tests/test_vib.py
+++ tests/test_vib.py
@@ -5,7 +5,7 @@ import vnbanks.banks.vib as vib
 class TestVIB(unittest.TestCase):
     def test_interest_rate(self):
         rate = vib.VIB()
-        self.assertTrue(rate.deposite_rate()['ONE_MONTH'] > 0)
+        self.assertTrue(rate.deposit_rate()['ONE_MONTH'] > 0)
 
 
 if __name__ == '__main__':
